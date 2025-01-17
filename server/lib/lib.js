4b01a17f093dea0cff818d47bd847db97ed3ea5c
Caspar van der Linden <c.vanderlinden@rubicon.nl>
2025-01-18 00:59:13 +0100

    ip check

--- server/lib/lib.js
+++ server/lib/lib.js
@@ -48,7 +48,7 @@ const getOS = () => {
         }
         else {
             log("stdout", stdout);
-            ipAddresses = stdout.split("\n");
+            ipAddresses = stdout;
         }
     });
 
